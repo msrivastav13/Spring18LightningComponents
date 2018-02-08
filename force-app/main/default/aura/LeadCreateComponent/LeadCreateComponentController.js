c4cb7190c67d4bee70c5994a77c365e6bfe55bdd
Mohith Shrivastava <mohith@codescience.com>
2018-02-07 21:31:19 -0500

    latest changes as per docs

--- force-app/main/default/aura/LeadCreateComponent/LeadCreateComponentController.js
+++ force-app/main/default/aura/LeadCreateComponent/LeadCreateComponentController.js
@@ -1,5 +1,11 @@
 ({
     handleSuccess : function(component, event, helper) {
+        console.log(event.getParams().response);
+        for (let key of Object.keys(event.getParams().response)) {
+            console.log(key + event.getParams().response[key]);
+          }
+        console.log(event.getParams().response.id);
+        //console.log(component.find("name").get("v.value"));
         var toastEvent = $A.get("e.force:showToast");
         toastEvent.setParams({
             "title": "Success!",
@@ -7,5 +13,16 @@
             "type": "success"
         });
         toastEvent.fire();
-    }
+    },
+
+    handleSubmit : function(component, event, helper) {
+        console.log('Submit Event' + JSON.stringify(event.getParams()));
+        console.log(component.find("company"));
+        component.find("company").set("v.fieldName","Company");
+        component.find("company").set("v.value",component.get("v.companyName"));
+    },
+
+    handleOnload : function(component, event, helper) {
+        console.log('Load Event' + JSON.stringify(event.getParams()));
+    },
 })
